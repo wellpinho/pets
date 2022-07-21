846556b90a302195d8470df235d2e04a6ae05e91
wellpinho <wellpinho@outlook.com>
2022-07-21 14:18:23 -0300

    new attr address in user model

---
+++ src/modules/users/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const User = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = User;
