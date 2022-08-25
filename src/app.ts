fffbbd43c13b872dbbf221530ba4129316bc7f1b
wellpinho <wellpinho@outlook.com>
2022-08-25 19:09:21 -0300

    server is running

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
-import express from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import "express-async-errors";
+import { CustomError } from "./errors/CustomError";
+import { routes } from "./routes";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.status(200).json("Bem vindo ao Adote um Pet.");
+});
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof CustomError) {
+    return res.status(error.statusCode).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "Error",
+    message: "Internal Server Error",
+  });
+});
+
+export { app };
