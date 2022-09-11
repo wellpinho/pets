b8238114b1f1def71e71ffe898f7b60ed2d1be23
wellpinho <wellpinho@outlook.com>
2022-09-11 14:22:46 -0300

    ✨ NEW FEATURE: edite pet

--- web/src/components/pets/add/index.jsx
+++ web/src/components/pets/add/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import api from "./../../../utils/api";
 import useFlashMessage from "../../../hooks/useFlashMessage";
 
--- web/src/components/pets/index.jsx
+++ web/src/components/pets/index.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import api from "./../../utils/api";
+import useFlshMessage from "../../hooks/useFlashMessage";
+import ImageAvatar from "../../components/imageAvatar";
 import "./pets.css";
 
 const MyPets = () => {
   const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlshMessage();
+
+  useEffect(() => {
+    api
+      .get("/pets/mypets", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(JSON.parse(token))}`,
+        },
+      })
+      .then((response) => {
+        setPets(response.data.pets);
+      });
+  }, [token]); // quando algo depende para continuar deve ser passado aqui
+
   return (
     <>
       <div className="alert alert-secondary" role="alert">
@@ -16,7 +34,27 @@ const MyPets = () => {
       </div>
       <div>
         {pets.length ? (
-          <div className="text-success">Meus pets cadastrados</div>
+          pets.map((pet) => {
+            <div key={pet._id}>
+              <ImageAvatar
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <strong>{pet.name}</strong>
+              <div className="actions">
+                {pet.available ? (
+                  <>
+                    {pet.adopter && <button>Cloncluir adoção</button>}
+                    <Link to={`/pets/edit/${pet._id}`}>Editar</Link>
+                    <button>Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet já foi adotado</p>
+                )}
+              </div>
+            </div>;
+          })
         ) : (
           <div className="text-danger">Não há pets cadastrados</div>
         )}
